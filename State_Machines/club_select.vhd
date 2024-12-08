b1de5822c9c814aae6b8c4aae8b9e4d1c6fef4e5
Jet4stream <158139019+Jet4stream@users.noreply.github.com>
2024-12-08 14:29:13 -0500

    Update club_select.vhd

--- State_Machines/club_select.vhd
+++ State_Machines/club_select.vhd
@@ -21,36 +21,34 @@ end;
 architecture synth of club_select is 
 -- 00 is a driver, 01 is a iron, 10 is a putter 
 signal slow_down_clock: unsigned(20 downto 0) := 21b"0"; -- changes how quickly the balls slow down
-signal speed: unsigned(4 downto 0); 
+signal speed: unsigned(4 downto 0) := "00000"; 
 
 begin
 	process (clk) begin
 		if rising_edge(clk) then
-			if (swing_speed = '00') then
-				speed <= "00000"
-			elsif
+			if (speed /= 0) then
 				if (current_c_state = "00") then -- driver
-					if (swing_speed = "00") -- high
+					if (swing_speed = "11") -- high
 						speed <= "11000";
-					elsif (swing_speed = "01") -- medium
+					elsif (swing_speed = "10") -- medium
 						speed <= "10110";
-					elsif (swing_speed = "10") -- low
+					elsif (swing_speed = "01") -- low
 						speed <= "10011";
 					end if;
 				elsif (current_c_state = "01") then -- iron
-					if (swing_speed = "00") -- high
+					if (swing_speed = "11") -- high
 						speed <= "10101";
-					elsif (swing_speed = "01") -- medium
+					elsif (swing_speed = "10") -- medium
 						speed <= "10010";
-					elsif (swing_speed = "10") -- low
+					elsif (swing_speed = "01") -- low
 						speed <= "01100";
 					end if;
 				elsif (current_c_state = "10") then -- putter
-					if (swing_speed = "00") -- high
+					if (swing_speed = "11") -- high
 						speed <= "01110";
-					elsif (swing_speed = "01") -- medium
+					elsif (swing_speed = "10") -- medium
 						speed <= "01011";
-					elsif (swing_speed = "10") -- low
+					elsif (swing_speed = "01") -- low
 						speed <= "01000";
 					end if;	
 				end if;
